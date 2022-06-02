81c396b86c13715230acac6fb85651917ebc9996
david caripunas <davidcaripunas7@gmail.com>
2022-06-02 20:55:30 -0300

    Finalizado Funcionalidade 1

--- js/script.js
+++ js/script.js
@@ -8,13 +8,46 @@ const app = Vue.createApp({
                 diaSemana:false,
                 desconto:false 
 
-            }
+            },
+            mensagem:{
+                'idade' : "Cálculo da idade a partir do ano de nascimento inserido pelo usuário",
+                'cambio': "Cálculo do valor em reais a partir de um valor em dólar e cotação inserida pelo usuário",
+                'diaSemana' : "Exibir o dia da semana a partir de uma data inserida pelo usuário",
+                'desconto' : "Calcular  o  resultado  a  partir  de  um  valor  base  e  o percentual de desconto indicados pelo usuário"
+            },
+            textoInicial:"Olá, seja bem vindo ao nosso sistema",
+            campoIdade:""
 
-        }
+       }
+
     },
+        
+        
+    
+    
     methods:{
         verificarLink(classe){
-            console.log(classe)
+            //console.log(classe[1])
+            let itens = classe[1] //classe é um parâmetro que irá conter um vetor com as classes da tag na qual a função está sendo chamada.
+
+            
+            // for..in permite percorrer um objeto
+            for (elementos in this.bloco){
+                //console.log(elementos)
+                console.log(elementos)
+                if(itens == elementos){
+                    this.bloco[elementos] = true
+                    this.textoInicial = this.mensagem[elementos]
+                }
+                else{
+                    this.bloco[elementos] = false
+                }
+            }
+        },
+        calcularIdade(){
+            let anoAtual = new Date().getFullYear()//para pegar o ano atual
+            let resposta = anoAtual - this.campoIdade
+            console.log(resposta)
         }
     }
 
